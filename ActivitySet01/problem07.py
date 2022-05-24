31e45924c335606647ec3848b13c96908c5fc286
abhireddy99 <94884686+abhireddy99@users.noreply.github.com>
2022-05-24 10:31:15 +0000

    commit

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,23 @@
-# Strings
-
-text = "X-DSPAM-Confidence:    0.8475"
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+       num = int (num)
+    except:
+        print("Invaild input")
+        continue
+    if smallest is None:
+        smallest = num
+    elif num < smallest:
+        smallest = num
+    if largest is None:
+        largest=num
+    elif num > largest:
+        largest = num
+print("Maximum is",largest)
+print("Minimum is",smallest) 
+        
+       
